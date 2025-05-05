dd292ea775b6a06ef9fb5ebaab7a3fb6ea41989c
symdec <simon.dcmbl@gmail.com>
2025-05-05 15:50:36 +0200

    update generator and dataset to use states

--- datasets/random_trajectory_generator.py
+++ datasets/random_trajectory_generator.py
@@ -1,37 +1,39 @@
 import random
 
-FILENAME = "simple_example.dat"
+STATE_FILE = "states.txt" # file when you can find the set of all possibles states, one per line
+OUTPUT_FILE = "simple_example.dat"
 N = 600
 
 # min and max number of points in a trajectory
 MIN_NB_POINTS = 1
 MAX_NB_POINTS = 20 
 
-# min and max coordinates of a point
-MIN_COORDINATE = 0
-MAX_COORDINATE = 10
+def get_states(state_file):
+    """
+    Get the list of all possible states (one per line) from a file.
+    """
+    states = []
+    with open(state_file, 'r') as file:
+        for line in file:
+            states.append(line.strip())
+    return states
 
-def generate_random_point():
-    """Generate a random point with coordinates (x, y)."""
-    x = round(random.uniform(MIN_COORDINATE, MAX_COORDINATE), 3)
-    y = round(random.uniform(MIN_COORDINATE, MAX_COORDINATE), 3)
-    return f"{x},{y}"
-
-def generate_trajectory(index, num_points):
-    """Generate a trajectory with the given index and number of points."""
-    points = [generate_random_point() for _ in range(num_points)]
+def generate_trajectory(index, num_states):
+    """Generate a trajectory with the given index and number of states."""
+    states = get_states(STATE_FILE)
+    points = [random.choice(states) for _ in range(num_states)]
     points_str = ";".join(points)
     trajectory = f"# {index}:\n>0:{points_str};"
     return trajectory
 
 def generate_trajectories(N):
-    """Generate N trajectories with varying number of points."""
-    # generate a random number of points for each trajectory
-    num_points_list = [random.randint(MIN_NB_POINTS, MAX_NB_POINTS) for _ in range(N)]
+    """Generate N trajectories with varying number of states."""
+    # generate a random number of states for each trajectory
+    num_states_list = [random.randint(MIN_NB_POINTS, MAX_NB_POINTS) for _ in range(N)]
     trajectories = []
     for i in range(N):
-        num_points = num_points_list[i] if i < len(num_points_list) else random.randint(MIN_NB_POINTS, MAX_NB_POINTS)
-        trajectory = generate_trajectory(i, num_points)
+        num_states = num_states_list[i] if i < len(num_states_list) else random.randint(MIN_NB_POINTS, MAX_NB_POINTS)
+        trajectory = generate_trajectory(i, num_states)
         trajectories.append(trajectory)
     return trajectories
 
@@ -43,6 +45,6 @@ def save_trajectories_to_file(trajectories, filename):
 
 if __name__ == "__main__":
     trajectories = generate_trajectories(N)
-    save_trajectories_to_file(trajectories, FILENAME)
+    save_trajectories_to_file(trajectories, OUTPUT_FILE)
 
-    print(f"Trajectories generated and saved to '{FILENAME}'.")
+    print(f"Trajectories generated and saved to '{OUTPUT_FILE}'.")
